669c2d1557881565ae49af324cfe41747168ea94
Thiago Alcantara <thiagosz63@gmail.com>
2025-01-24 00:53:01 -0300

    Home screen created

---
+++ src/main/java/application/Main.java
@@ -0,0 +1,28 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/css/MainCSS.css")).toExternalForm());
+        stage.setTitle("My Play Music");
+        stage.setScene(scene);
+        Image icon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/image/simboloMusical.png")));
+        stage.getIcons().add(icon);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
