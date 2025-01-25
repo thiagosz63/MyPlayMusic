6d28e534335527dacaeb42c907401c0d902e4498
Thiago Alcantara <thiagosz63@gmail.com>
2025-01-25 02:44:03 -0300

    features

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -11,10 +12,15 @@ import java.io.IOException;
 import java.util.Objects;
 
 public class Main extends Application {
+
+    private static double xOffset = 0;
+    private static double yOffset = 0;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
+        AnchorPane root = fxmlLoader.load();
+        Scene scene = new Scene(root);
         scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/css/MainCSS.css")).toExternalForm());
         stage.setTitle("My Play Music");
         stage.setScene(scene);
@@ -22,6 +28,15 @@ public class Main extends Application {
         stage.getIcons().add(icon);
         stage.initStyle(StageStyle.UNDECORATED);
         stage.show();
+
+        root.setOnMouseDragged(event -> {
+            stage.setX(event.getScreenX() - xOffset);
+            stage.setY(event.getScreenY() - yOffset);
+        });
+        root.setOnMousePressed(event -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
     }
 
     public static void main(String[] args) {
--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,21 +1,33 @@
 package controller;
 
+import javafx.animation.TranslateTransition;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.stage.Stage;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.image.ImageView;
+import javafx.util.Duration;
 import utils.Utils;
 
-public class MainController {
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.ResourceBundle;
 
-    private static double xOffset = 0;
-    private static double yOffset = 0;
+public class MainController implements Initializable {
 
+    private @FXML MediaView mediaView;
     private @FXML ImageView audio;
     private @FXML Label currentTime;
     private @FXML Label musicName;
@@ -23,6 +35,117 @@ public class MainController {
     private @FXML AnchorPane telaAPP;
     private @FXML Label timeTotal;
     private @FXML Slider volumeSelector;
+
+    private MediaPlayer mediaPlayer;
+    private List<String> musics = new ArrayList<>();
+    private int currentMusicIndex;
+    private double volume = 30;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        volumeSelector.setValue(volume);
+        uploadMusic();
+        formatMusicName();
+        musicMoveName();
+        timeSet();
+        changeVolume();
+    }
+
+    private void timeSet() {
+        mediaPlayer.setOnReady(() -> {
+            Duration durationTotalTimeMusic = mediaPlayer.getTotalDuration();
+            int TotalSeconds = (int) durationTotalTimeMusic.toSeconds();
+            int minutes = TotalSeconds / 60;
+            int seconds = TotalSeconds % 60;
+            timeTotal.setText(String.format("%02d:%02d", minutes, seconds));
+        });
+
+        musicTime.valueProperty().addListener((
+                observable, oldValue, newValue) -> {
+            if (musicTime.isValueChanging()) {
+                double position = newValue.doubleValue() / 100;
+                changeMusicTempo(position);
+            }
+        });
+
+        mediaPlayer.currentTimeProperty().addListener((
+                        observable, oldValue, newValue) -> {
+                    Duration DurationCurrentTime = mediaPlayer.getCurrentTime();
+                    int currentSeconds = (int) DurationCurrentTime.toSeconds();
+                    int minutes = currentSeconds / 60;
+                    int seconds = currentSeconds % 60;
+                    currentTime.setText(String.format("%02d:%02d", minutes, seconds));
+
+                    if (!musicTime.isValueChanging()) {
+                        musicTime.setValue(newValue.toSeconds() /
+                                mediaPlayer.getTotalDuration().toSeconds() * 100);
+                    }
+                }
+        );
+        mediaPlayer.setOnEndOfMedia(this::next);
+    }
+
+    private void changeMusicTempo(double position) {
+        Duration duration = mediaPlayer.getTotalDuration();
+        Duration newDuration = duration.multiply(position);
+        mediaPlayer.seek(newDuration);
+    }
+
+    private void musicMoveName() {
+        TranslateTransition transition = new TranslateTransition(Duration.seconds(30), musicName);
+        transition.setFromX(350);
+        transition.setToX(-670);
+        transition.setCycleCount(TranslateTransition.INDEFINITE);
+        transition.play();
+    }
+
+    private void formatMusicName() {
+        String name = musics.get(currentMusicIndex).replace("/music/", "");
+        musicName.setText(name);
+    }
+
+    private void uploadMusic() {
+        musics.add("/music/Track01.mp3");
+        musics.add("/music/Track02.mp3");
+        currentMusicIndex = 0;
+        String currentMusic = musics.get(currentMusicIndex);
+        Media media = new Media(Objects.requireNonNull(getClass().getResource(currentMusic)).toExternalForm());
+        mediaPlayer = new MediaPlayer(media);
+        mediaView.setMediaPlayer(mediaPlayer);
+        mediaPlayer.setVolume(volume);
+    }
+
+    private void changeCurrentMusic() {
+        String currentMusic = musics.get(currentMusicIndex);
+        Media media = new Media(Objects.requireNonNull(getClass().getResource(currentMusic)).toExternalForm());
+        mediaPlayer = new MediaPlayer(media);
+        mediaView.setMediaPlayer(mediaPlayer);
+        timeSet();
+        formatMusicName();
+        player();
+    }
+
+    private void changeVolume() {
+        volumeSelector.valueProperty().addListener((
+                        observable, oldValue, newValue) -> {
+    volume = newValue.doubleValue() / 100;
+    mediaPlayer.setVolume(volume);
+
+    if(volume!= 0){
+        mediaPlayer.setMute(false);
+    }
+    audio.setImage(volume > 0 ? new Image(Objects.requireNonNull(getClass().getResource("/image/audioOn.png")).toExternalForm()) :
+        new Image(Objects.requireNonNull(getClass().getResource("/image/audioOff.png")).toExternalForm()));
+                }
+        );
+    }
+    private void checkMute(){
+        if(volume == 0){
+            mediaPlayer.setMute(true);
+            mediaPlayer.setVolume(0);
+        }
+    }
 
     @FXML
     private void minimize(MouseEvent mouseEvent) {
@@ -36,33 +159,52 @@ public class MainController {
     }
 
     @FXML
-    private void telaAPPDragged(MouseEvent mouseEvent) {
-        Stage stage = Utils.currentStage(mouseEvent);
-        stage.setX(mouseEvent.getScreenX() - xOffset);
-        stage.setY(mouseEvent.getScreenY() - yOffset);
+    private void player() {
+        mediaPlayer.play();
+        checkMute();
     }
 
     @FXML
-    private void telaAppPressed(MouseEvent mouseEvent) {
-        xOffset = mouseEvent.getSceneX();
-        yOffset = mouseEvent.getSceneY();
+    private void pause(MouseEvent mouseEvent) {
+        mediaPlayer.pause();
     }
 
-    public void preview(MouseEvent mouseEvent) {
+    @FXML
+    private void stop() {
+        mediaPlayer.stop();
     }
 
-    public void player(MouseEvent mouseEvent) {
+    @FXML
+    private void preview() {
+        stop();
+        currentMusicIndex--;
+        if (currentMusicIndex < 0) {
+            currentMusicIndex = musics.size() - 1;
+        }
+        changeCurrentMusic();
     }
 
-    public void pause(MouseEvent mouseEvent) {
+    @FXML
+    private void next() {
+        stop();
+        currentMusicIndex++;
+        if (currentMusicIndex >= musics.size()) {
+            currentMusicIndex = 0;
+        }
+        changeCurrentMusic();
     }
 
-    public void stop(MouseEvent mouseEvent) {
-    }
-
-    public void next(MouseEvent mouseEvent) {
-    }
-
-    public void mute(MouseEvent mouseEvent) {
+    @FXML
+    private void mute() {
+        if (mediaPlayer.isMute()) {
+            mediaPlayer.setMute(false);
+            audio.setImage(new Image(Objects.requireNonNull(getClass().getResource("/image/audioOn.png")).toExternalForm()));
+      mediaPlayer.setVolume(volumeSelector.getValue());
+      volume = volumeSelector.getValue();
+        } else {
+            mediaPlayer.setMute(true);
+            audio.setImage(new Image(Objects.requireNonNull(getClass().getResource("/image/audioOff.png")).toExternalForm()));
+            volume = 0;
+        }
     }
 }
